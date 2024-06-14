9b0d65db750461dea4e1e0475ceebb819990347b
bong <m.wining.bong@icloud.com>
2024-06-14 00:47:20 +0000

    Implement inheritance and method overriding

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dcit318Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inheritance and Method Overriding
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound(); // Output: Some generic sound
+            dog.MakeSound(); // Output: Bark
+            cat.MakeSound(); // Output: Meow
+        }
+    }
+}
